f9a7e5c5fd53628e7429d07e428c8273df2962cb
joehmchiu <joehmchiu@gmail.com>
2018-11-10 21:12:11 -0800

    Update jenkins-jobs.groovy

--- lib/jenkins-jobs.groovy
+++ lib/jenkins-jobs.groovy
@@ -1,47 +1,59 @@
-node {
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import java.net.URL
 
-    project = "cloud-platforms"
-    script = "stop-vms.yml"
-    def task = "gcp"
-    def workspace = pwd()
-    def repositoryUrl = "https://github.com/joehmchiu/${project}.git"
-    def branch = "master"
-    def msg = "VMs powered off"
+// loading and running jenkins tasks
 
-    withEnv(["PATH+ANSIBLE=${tool 'ansible-2.4.2.0'}"]) {
+stage 'Confirm Task'
+node() {
+    timeout(time: 30, unit: 'SECONDS') {
+        input "${msg}?"
+    }
+}
 
-        stage 'Confirm'
-        timeout(time: 30, unit: 'SECONDS') {
-            input "${msg}?"
-        }
-        
-        stage 'Init Working Env'
+stage 'Init Working Env'
+node() {
+        echo "hello world"
         sh "sudo /opt/bin/init.sh ${project} ${task}"
+}
 
-        stage 'Check List'
-        echo "\u2600 workspace=${workspace}/"
-        sh "ls -ltrh ${workspace}"
+stage 'Check List'
+node() {
+        sh "ls -ltrh /tmp/env/"
+}
 
-        stage 'Check Ansible Availability'
+stage 'Check Ansible Availability'
+node() {
         sh 'which ansible'
+}
 
-        stage 'Check Ansible Version'
+stage 'Check Ansible Version'
+node() {
         sh 'ansible --version'
+}
 
-        stage 'SCM Update'
+stage 'SCM Update'
+node() {
         git url: repositoryUrl, branch: branch
         sh "sudo cp -rf ./${task}/* /opt/${project}/${task}/."
+}
 
-        stage 'Run Playbook Tasks'
+stage 'Run Playbook Tasks'
+node() {
+    withEnv(["PATH+ANSIBLE=${tool 'ansible-2.4.2.0'}"]) {
         ansiblePlaybook (
             playbook: "/opt/${project}/${task}/${script}",
             sudo: true
         )
-
-        stage "Tasks Logged"
-        sh "sudo /opt/bin/log.sh '${msg}'"
-        
-        stage 'Finalize Task'
-        sh "sudo /opt/bin/fin.sh ${project} ${task}"        
     }
 }
+
+stage "Tasks Finalized"
+node() {
+        sh "sudo /opt/bin/log.sh '${msg}'"
+        sh "sudo /opt/bin/fin.sh ${project} ${task}"
+}
+
